18f2179a575f63c65615f2883fb8ded49ae382aa
redpois0n <git@redpois0n>
2013-05-06 14:34:08 +0200

    OSX LAF screenmenubar

--- src/com/redpois0n/rdp/Main.java
+++ src/com/redpois0n/rdp/Main.java
@@ -26,6 +26,7 @@ public class Main {
 	public static final long delay = 1000;
 
 	public static void main(String[] args) throws Exception {	
+		System.setProperty("apple.laf.useScreenMenuBar", "true");
 		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		
 		Frame frame = new Frame();
