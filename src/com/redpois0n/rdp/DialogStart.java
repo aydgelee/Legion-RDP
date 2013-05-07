4f5c8b2f2d71b4b4ba4cfb9278b489c78de29bce
redpois0n <git@redpois0n>
2013-05-07 16:52:35 +0200

    Can start, instructions changed

--- src/com/redpois0n/rdp/DialogStart.java
+++ src/com/redpois0n/rdp/DialogStart.java
@@ -8,6 +8,9 @@ import javax.swing.JComboBox;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JSpinner;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 @SuppressWarnings("serial")
 public class DialogStart extends JDialog {
@@ -28,6 +31,15 @@ public class DialogStart extends JDialog {
 		
 		spinner = new JSpinner();
 		spinner.setValue(Runtime.getRuntime().availableProcessors());
+		
+		JButton btnStart = new JButton("Start");
+		btnStart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				for (int i = 0; i < (Integer) spinner.getValue(); i++) {
+					new Thread(new Cracker()).start();
+				}
+			}
+		});
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -40,7 +52,11 @@ public class DialogStart extends JDialog {
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
 						.addComponent(spinner, GroupLayout.PREFERRED_SIZE, 56, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(107, Short.MAX_VALUE))
+					.addContainerGap(196, Short.MAX_VALUE))
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap(345, Short.MAX_VALUE)
+					.addComponent(btnStart)
+					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -53,7 +69,9 @@ public class DialogStart extends JDialog {
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblThreads)
 						.addComponent(spinner, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap(215, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.RELATED, 178, Short.MAX_VALUE)
+					.addComponent(btnStart)
+					.addContainerGap())
 		);
 		getContentPane().setLayout(groupLayout);
 
