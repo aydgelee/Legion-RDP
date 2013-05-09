c2b1730d54c98ac883039948791d75af432b644b
redpois0n <git@redpois0n>
2013-05-09 11:24:47 +0200

    Added bin

--- src/com/redpois0n/rdp/CrackLogger.java
+++ src/com/redpois0n/rdp/CrackLogger.java
@@ -1,5 +1,12 @@
 package com.redpois0n.rdp;
 
-public class CrackLogger {
+import org.apache.log4j.Logger;
+
+public class CrackLogger extends Logger {
+
+	protected CrackLogger(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
 
 }
